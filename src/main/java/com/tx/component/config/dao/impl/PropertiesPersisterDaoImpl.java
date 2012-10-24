e4ef000d7bab592d0ee862d556c36d4973e9d421
240638006 <240638006@qq.com>
2012-10-24 18:35:34 +0800

    xx

--- src/main/java/com/tx/component/config/dao/impl/PropertiesPersisterDaoImpl.java
+++ src/main/java/com/tx/component/config/dao/impl/PropertiesPersisterDaoImpl.java
@@ -6,21 +6,29 @@
  */
 package com.tx.component.config.dao.impl;
 
+import java.util.HashMap;
+import java.util.Map;
 
- /**
-  * <功能简述>
-  * <功能详细描述>
-  * 
-  * @author  PengQingyang
-  * @version  [版本号, 2012-10-24]
-  * @see  [相关类/方法]
-  * @since  [产品/模块版本]
-  */
+import javax.sql.DataSource;
+
+import org.springframework.jdbc.datasource.embedded.ConnectionProperties;
+import org.springframework.jdbc.datasource.embedded.DataSourceFactory;
+
+/**
+ * <属性持久类>
+ * <功能详细描述>
+ * 
+ * @author  PengQingyang
+ * @version  [版本号, 2012-10-24]
+ * @see  [相关类/方法]
+ * @since  [产品/模块版本]
+ */
 public class PropertiesPersisterDaoImpl {
     
-    /** <默认构造函数> */
-    public PropertiesPersisterDaoImpl() {
-        // TODO Auto-generated constructor stub
-    }
+    /** datasource Map */
+    private Map<String, DataSource> datasourceMap = new HashMap<String, DataSource>();
+    
+    private boolean isSupportP6spy = false;
+    
     
 }
